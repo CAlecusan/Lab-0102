7406ab66ff38e431b421903c7f5a6e2adb0376f9
CAlecusan <carell.theo@gmail.com>
2026-05-18 10:52:47 +0300

    Refactor filter even number

--- src/main/java/com/parking/Main.java
+++ src/main/java/com/parking/Main.java
@@ -9,10 +9,12 @@ import static java.util.Locale.filter;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1,2,3, 4,5, 6,7,8,9,10,11, 12);
-        numbers.stream() // se instantiaza obiect de tipul stream
-                .filter(num -> num % 2 == 0)
-                .forEach(number -> System.out.println(number));
+        Void FilterEvenNumber; {
+            List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+            numbers.stream() // se instantiaza obiect de tipul stream
+                    .filter(num -> num % 2 == 0)
+                    .forEach(System.out::println);
+        }
 
 
 
